2fc76af0be712958a699518a137acd72cdb0b447
HorvathIstvan9 <71818240+HorvathIstvan9@users.noreply.github.com>
2020-11-23 16:23:19 +0100

    Added PlayerMovement script

--- Project/Pac-Man/Assets/Scripts/Entity.cs
+++ Project/Pac-Man/Assets/Scripts/Entity.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    [SerializeField]
     private float moveSpeed;
 
     public float MoveSpeed
---
+++ Project/Pac-Man/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool left = false; bool right = false;
+    public Rigidbody playerrb;
+    public Player player;
+
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKey("a"))
+        {
+            left = true;
+            right = false;
+        }
+        else if (Input.GetKey("d"))
+        {
+            right = true;
+            left = false;
+        }
+        //playerrb.velocity = new Vector3(playerrb.velocity.x, playerrb.velocity.y, 1) * player.MoveSpeed * Time.deltaTime;
+        playerrb.transform.Translate(Vector3.forward * 1 * player.MoveSpeed * Time.deltaTime);
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Turningpoint"))
+        {
+            if (left)
+            {
+                playerrb.transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+            }
+            else if (right)
+            {
+                playerrb.transform.Rotate(new Vector3(0, 90, 0), Space.Self);
+            }
+            right = false;
+            left = false;
+        }
+    }
+}
