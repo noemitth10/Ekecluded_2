ea5a85c04fb6be18ef4e1e561e5c1349912d69f1
HorvathIstvan9 <71818240+HorvathIstvan9@users.noreply.github.com>
2020-11-24 19:11:26 +0100

    Fixes in movement script

--- Project/Pac-Man/Assets/Scripts/PlayerMovement.cs
+++ Project/Pac-Man/Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,32 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody playerrb;
     public Player player;
     private bool canMove = true;
+    private bool canMoveLeft = true;
+    private bool canMoveRight = true;
 
     void FixedUpdate()
     {
         
         if (Input.GetKey("a"))
         {
-            left = true;
-            right = false;
+            if (canMoveLeft)
+            {
+                left = true;
+                right = false;
+                canMoveLeft = true;
+                canMoveRight = true;
+            }
+           
         }
         else if (Input.GetKey("d"))
         {
-            right = true;
-            left = false;
+            if (canMoveRight)
+            {
+                left = false;
+                right = true;
+                canMoveLeft = true;
+                canMoveRight = true;
+            }
         }
         //playerrb.velocity = new Vector3(playerrb.velocity.x, playerrb.velocity.y, 1) * player.MoveSpeed * Time.deltaTime;
         if (canMove)
@@ -31,42 +44,79 @@ public class PlayerMovement : MonoBehaviour
 
 
         RaycastHit hit;
+        RaycastHit lefthit;
+        RaycastHit righthit;
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10,1<<8))
         {
-            if (!right&&!left)
+
+            canMove = false;
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out righthit, 10, 1 << 8))
             {
-                canMove = false;
-                Debug.Log("can't move");
+                    //canMove = true;
+                    canMoveLeft = true;
+                    canMoveRight = false;
+                    right = false;
             }
-            else
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out lefthit, 10, 1 << 8))
+            {
+                    //canMove = true;
+                    canMoveRight = true;
+                    canMoveLeft = false;
+                left = false;
+            }
+            if (right || left)
             {
                 canMove = true;
             }
-           
+
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log(hit.collider.name);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.yellow);
+
+        }
+        else
+        {
+            canMove = true;
+            canMoveRight = true;
+            canMoveLeft = true;
+            
         }
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 200, Color.yellow);
+        Debug.Log(string.Format("balra: {0} jobbra: {1}", canMoveLeft, canMoveRight));
     }
     private void OnTriggerEnter(Collider other)
     {
         
         if (other.CompareTag("Turningpoint"))
         {
-           
+            RaycastHit lefthit;
+            RaycastHit righthit;
+            Debug.Log("fordulópont");
             if (left)
             {
-                playerrb.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 1.5f, other.transform.position.z);
-                playerrb.transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+                if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out lefthit, 10, 1 << 8))
+                {
+                    playerrb.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 1.5f, other.transform.position.z);
+                    playerrb.transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+                }
+            
+              
             }
             else if (right)
             {
-                playerrb.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 1.5f, other.transform.position.z);
-                playerrb.transform.Rotate(new Vector3(0, 90, 0), Space.Self);
+                if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out righthit, 10, 1 << 8))
+                {
+                    playerrb.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + 1.5f, other.transform.position.z);
+                    playerrb.transform.Rotate(new Vector3(0, 90, 0), Space.Self);
+                }
+                    
             }
             right = false;
             left = false;
         }
     }
+    
+    
 }
