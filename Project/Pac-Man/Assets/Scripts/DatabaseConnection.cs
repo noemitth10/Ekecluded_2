5ed43df7df920e520a1eeb29f60f7ae95437ece2
HorvGabor <64591168+HorvGabor@users.noreply.github.com>
2020-11-25 21:55:29 +0100

    Fixed Database Connection

---
+++ Project/Pac-Man/Assets/Scripts/DatabaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oracle.ManagedDataAccess.Client;
+
+public class DatabaseConnection : MonoBehaviour
+{
+    public static OracleConnection GetConnection()
+    {
+        OracleConnection connection = new OracleConnection();
+        connection.ConnectionString = @"Data Source=193.225.33.71;User Id=EB6SAR;Password=EKE2020;";
+
+        return connection;
+    }
+}
