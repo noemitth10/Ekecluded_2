6aca962003a08c9217b5ce535cd3e8b3e4ba1965
HorvGabor <64591168+HorvGabor@users.noreply.github.com>
2020-11-25 21:58:50 +0100

    Created Leaderboard class

---
+++ Project/Pac-Man/Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    private int id;
+    private string playerName;
+    private int score;
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string PlayerName
+    {
+        get
+        {
+            return playerName;
+        }
+
+        set
+        {
+            playerName = value;
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            score = value;
+        }
+    }
+
+    public Leaderboard(int id, string playerName, int score)
+    {
+        this.Id = id;
+        this.PlayerName = playerName;
+        this.Score = score;
+    }
+
+    public Leaderboard()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        string row = string.Format("Player: {0} Score: {1}", PlayerName, Score);
+
+        return row;
+    }
+}
