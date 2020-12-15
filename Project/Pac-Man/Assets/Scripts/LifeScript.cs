706477e51c6b6bc19f4a755ea46b9719857458d7
HorvGabor <64591168+HorvGabor@users.noreply.github.com>
2020-12-15 18:49:13 +0100

    Added player UI and fixed ghost lightning

---
+++ Project/Pac-Man/Assets/Scripts/LifeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeScript : MonoBehaviour
+{
+    public Text playerLife;
+    public Player player;
+
+    public void Update()
+    {
+        playerLife.text = "Health: " + player.GetComponent<Player>().Life.ToString();
+    }
+}
---
+++ Project/Pac-Man/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text playerScore;
+    public Player player;
+    
+    public void Update()
+    {
+        playerScore.text = "Score: " + player.GetComponent<Player>().Points.ToString();
+    }
+}
